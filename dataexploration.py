4755f662cd9780dee8b6ea2710e8f32b6a7223b1
m2waters <mwaters2110@gmail.com>
2024-12-09 14:10:15 +0000

    added intial exploration of dataset features

--- dataexploration.py
+++ dataexploration.py
@@ -3,3 +3,14 @@ import pandas as pd
 df = pd.read_csv("insurance.csv")
 
 print(df.head())
+
+print(df.describe())
+print(df.columns)
+
+df_female = df[df['sex'] == "female"]
+df_male = df[df['sex'] == 'male']
+
+print(df_female.head())
+print(df_male.head())
+
+print(f'There are {len(df_male)} male data points and {len(df_female)} female datapoints')
